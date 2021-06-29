347713221b695714a69500ef5a82c25068bd3a61
adam <shembeku@gmail.com>
2021-06-30 01:07:09 +0300

    Users can see surveys results

---
+++ client/src/components/surveys/surveyList.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.reverse().map(surveys => {
+      return (
+        <div className="card darken-1" key={surveys._id}>
+          <div class="card-content text-white">
+            <span class="card-title">{surveys.title}</span>
+            <p>
+              {surveys.body}
+            </p>
+            <p className="right">
+              Sent 0n: {new Date(surveys.dateSent).toLocaleDateString()}
+            </p>
+            </div>
+          <div className="card-action">
+            <a>Yes: {surveys.yes}</a>
+            <a>No: {surveys.no}</a>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderSurveys()}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ surveys }) {
+  return { surveys }
+}
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
