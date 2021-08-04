b150ead21bfe57e96558c462e89b5aa9eb9c785e
adam <shembeku@gmail.com>
2021-08-04 11:49:44 +0300

    first commit

--- client/src/components/surveys/surveyList.js
+++ client/src/components/surveys/surveyList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { fetchSurveys } from '../../actions';
+import Card from 'react-bootstrap/Card';
+import ProgressBar from "react-bootstrap/ProgressBar";
 
 class SurveyList extends Component {
   componentDidMount() {
@@ -10,21 +12,62 @@ class SurveyList extends Component {
   renderSurveys() {
     return this.props.surveys.reverse().map(surveys => {
       return (
-        <div className="card darken-1" key={surveys._id}>
-          <div className="card-content text-white">
-            <span className="card-title">{surveys.title}</span>
-            <p>
-              {surveys.body}
-            </p>
-            <p className="right">
-              Sent 0n: {new Date(surveys.dateSent).toLocaleDateString()}
-            </p>
+        //   <div className="card darken-1" key={surveys._id}>
+        //     <div className="card-content text-white">
+        //       <span className="card-title">{surveys.title}</span>
+        //       <p>
+        //         {surveys.body}
+        //       </p>
+        //       <p className="right">
+        //         Sent 0n: {new Date(surveys.dateSent).toLocaleDateString()}
+        //       </p>
+        //     </div>
+        //     <div className="card-action">
+        // <div>Yes: {surveys.yes}
+        //   <div className="progress" style={{ maxWidth: '300px' }}>
+        //     <div className="determinate" style={{ width: surveys.yes / (surveys.yes + surveys.no) * 300 }}>
+
+        //     </div>
+        //   </div>
+        // </div>
+        //       <div>No: {surveys.no}
+        //         <div className="progress" style={{ maxWidth: '300px' }}>
+        //           <div className="determinate" style={{ width: surveys.no / (surveys.yes + surveys.no) * 300 }}>
+
+        //           </div>
+        //         </div>
+        //       </div>
+        //       <a className="right" style={{marginBottom: '20px'}}>
+        //         Delete
+        //       </a>
+        //     </div>
+        //   </div>
+        <Card key={surveys._id} style={{ padding: '20px', maxWidth: '700px'}}>
+          <Card.Body>
+            <Card.Title>{surveys.title}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              <p>
+                {surveys.body}
+              </p>
+              <p className="right">
+                Sent 0n: {new Date(surveys.dateSent).toLocaleDateString()}
+              </p>
+            </Card.Subtitle>
+            <div>
+            <div style={{ maxWidth: '300px' }}>
+              Yes: {surveys.yes}
+              <ProgressBar variant="success" now={surveys.yes / (surveys.yes + surveys.no) * 100} />
             </div>
-          <div className="card-action">
-            <a>Yes: {surveys.yes}</a>
-            <a>No: {surveys.no}</a>
-          </div>
-        </div>
+            <div >
+              No: {surveys.no}
+              <ProgressBar variant="info" now={surveys.no / (surveys.yes + surveys.no) * 100} />
+            </div>
+            </div>
+            <div style={{ paddingBottom: '10px' }}>
+              <a className="right" >Delete Survey</a>
+            </div>
+          </Card.Body>
+        </Card>
       )
     })
   }
